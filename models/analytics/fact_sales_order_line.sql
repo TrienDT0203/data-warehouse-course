9c5771744fa6f9e4cb6c74a4eb64851c02ee5f3b
TrienDT0203 <bp_trien_dinh@colpal.com>
2023-09-07 17:50:19 +0700

    modify profile.yaml, add dbt-env

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,3 +1,5 @@
-SELECT 
-  *
+SELECT order_line_id as	sales_order_line_key
+    , quantity
+    , unit_price
+    , quantity * unit_price as gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
