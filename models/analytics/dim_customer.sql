273ff49126329eccce36ebfe6d2e367f7922bdc9
TrienDT0203 <bp_trien_dinh@colpal.com>
2023-09-09 17:36:53 +0700

    finish dim_customer table, add stg_fact_sales_order table and update fact_sales_order_line table

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,12 @@
-SELECT  customer_id as customer_key
-      , customer_name 
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+with customers__source as (
+  SELECT  *
+  FROM `vit-lam-data.wide_world_importers.sales__customers`
+),
+customers_rename_col as (
+  SELECT  cast(customer_id as int) customer_key
+      , cast(customer_name as string) customer_name 
+  FROM customers__source
+)
+SELECT customer_key
+    , customer_name 
+FROM customers_rename_col
