b9a4dffa2d7482c70a05a332152da4f240cde358
TrienDT0203 <bp_trien_dinh@colpal.com>
2023-09-12 19:57:09 +0700

    modify dim_product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,12 +1,19 @@
-with customers__source as (
-  SELECT  *
-  FROM `vit-lam-data.wide_world_importers.sales__customers`
+with source_warehouse_stock_items as (
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
 ),
-customers_rename_col as (
-  SELECT  cast(customer_id as int) customer_key
-      , cast(customer_name as string) customer_name 
-  FROM customers__source
+source_warehouse_stock_items__rename_col as (
+  SELECT stock_item_id as product_key
+      , stock_item_name
+  FROM source_warehouse_stock_items
+),
+source_warehouse_stock_items__cast_type as (
+  SELECT cast(product_key as int) product_key
+      , cast(stock_item_name as string) product_name
+  FROM source_warehouse_stock_items__rename_col
 )
-SELECT customer_key
-    , customer_name 
-FROM customers_rename_col
+
+SELECT product_key
+      , product_name
+from source_warehouse_stock_items__cast_type
