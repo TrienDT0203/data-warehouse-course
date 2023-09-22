34e3ef8ad8a7968bdda698c04c2c463f501d16ab
TrienDT0203 <bp_trien_dinh@colpal.com>
2023-09-22 21:31:11 +0700

    add commit

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,43 +2,33 @@ with fact_sales_order_line as (
   SELECT *
   FROM `vit-lam-data.wide_world_importers.sales__order_lines`
 ),
-fact_sales_order_line__renamed_column as (
-  SELECT order_line_id as sales_order_line_key
-      , order_id as sales_order_id_key
-      , stock_item_id as  product_key
-      , quantity
-      , unit_price
-      , quantity * unit_price as gross_amount
-  FROM fact_sales_order_line
-),
-fact_sales_order_line__cast_type as (
-  SELECT cast(sales_order_line_key as int) sales_order_line_key
-        , sales_order_id_key
-        , product_key
+  fact_sales_order_line__renamed_column as (
+    SELECT order_line_id as sales_order_line_key
+        , order_id as sales_order_id_key
+        , stock_item_id as  product_key
         , quantity
         , unit_price
         , quantity * unit_price as gross_amount
-  FROM fact_sales_order_line__renamed_column
-),
-fact_sales_order_line__join_stg_sales_order as (
-  SELECT sales_order_line_key
-      , tb1.sales_order_id_key
-      , tb2.customer_key
-      , tb2.picked_by_person_key
-      , product_key
-      , quantity
-      , unit_price
-      , gross_amount
-  FROM fact_sales_order_line__cast_type tb1 
-  JOIN {{ref('stg_fact_sales_order')}} tb2
-    ON tb1.sales_order_id_key = tb2.sales_order_key
-)
-select sales_order_line_key
-      , sales_order_id_key
-      , coalesce(customer_key,-1) customer_key
-      , picked_by_person_key
-      , product_key
-      , quantity
-      , unit_price
-      , gross_amount
-from fact_sales_order_line__join_stg_sales_order
+    FROM fact_sales_order_line
+  ),
+  fact_sales_order_line__cast_type as (
+    SELECT cast(sales_order_line_key as int) sales_order_line_key
+          , sales_order_id_key
+          , product_key
+          , quantity
+          , unit_price
+          , quantity * unit_price as gross_amount
+    FROM fact_sales_order_line__renamed_column
+  ),
+
+SELECT fact_order_line.sales_order_line_key
+    , fact_order_line.sales_order_id_key
+    , coalesce(fact_order_header.customer_key,-1) customer_key
+    , coalesce(fact_order_header.picked_by_person_key,-1) picked_by_person_key
+    , fact_order_line.product_key
+    , quantity
+    , unit_price
+    , gross_amount
+FROM fact_sales_order_line__cast_type fact_order_line 
+JOIN {{ref('stg_fact_sales_order')}} fact_order_header
+  ON fact_order_line.sales_order_id_key = fact_order_header.sales_order_key
