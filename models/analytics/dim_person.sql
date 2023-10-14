1aea19fbd12d4468b37ed9ad0d55e10e2bde3faf
TrienDT0203 <bp_trien_dinh@colpal.com>
2023-10-14 16:17:32 +0700

    use dbt ref instead of copy dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -6,25 +6,57 @@ with application_people__source as (
 dim_people_rename_col as (
   SELECT person_id as people_key
       , full_name as full_name
+      , search_name 
+      , is_employee
+      , is_salesperson
   FROM application_people__source
 ),
 dim_people_cast_type as (
   SELECT cast(people_key as int) people_key  
         , cast(full_name as string) full_name
+        , cast(search_name as string) search_name  
+        , cast(is_employee as boolean) is_employee 
+        , cast(is_salesperson as boolean) is_salesperson 
+  FROM dim_people_rename_col
+),
+dim_people_case_when as (
+  SELECT  people_key  
+        , full_name
+        , search_name  
+        , case 
+            when is_employee = true then "Employee"
+            when  is_employee = false then "Not employee"
+            else "Undefined"
+          end as is_employee
+        , case 
+            when is_salesperson = true then "Salesperson"
+            when  is_salesperson = false then "Not salesperson"
+            else "Undefined"
+          end as is_salesperson
   FROM dim_people_rename_col
 ),
 dim_people_union as (
   select *
-  from dim_people_cast_type
+  from dim_people_case_when
+
   union all 
   select 0 as people_key
       , "Undefined" as full_name
+      , "Undefind" as search_name
+      , "Undefind" as is_employee
+      , "Undefind" as is_salesperson
+
   union all 
   select -1 as people_key
       , "Error/ Invalid" as full_name
-      
+      , "Error/ Invalid" as search_name
+      , "Error/ Invalid" as is_employee
+      , "Error/ Invalid" as is_salesperson
 )
 
 SELECT people_key
       , full_name
+      , search_name
+      , is_employee
+      , is_salesperson
 FROM dim_people_union
--- models/analytics/role_playing_dimensions/dim_salesperson_person.sql
+++ models/analytics/role_playing_dimensions/dim_salesperson_person.sql
@@ -1,32 +1,5 @@
-with source_people as (
-  SELECT 
-    *
-  FROM `vit-lam-data.wide_world_importers.application__people`
-),
-dim_people_rename_col as (
-  SELECT person_id as people_key
-      , full_name as full_name
-  FROM source_people
-),
-dim_people_cast_type as (
-  SELECT cast(people_key as int) people_key  
-        , cast(full_name as string) full_name
-  FROM dim_people_rename_col
-),
-dim_people_union as (
-  select *
-  from dim_people_cast_type
-
-  union all 
-  select 0 as people_key
-      , "Undefined" as full_name
-
-  union all 
-  select -1 as people_key
-      , "Error/ Invalid" as full_name
-      
-)
-
-SELECT people_key as salesperson_key
-      , full_name as salesperson_full_name
-FROM dim_people_union
+SELECT 
+      people_key as salesperson_person_key 
+    , full_name as salesperson_person_name 
+    , search_name as salesperson_search_name 
+FROM {{ ref('dim_person')}}
